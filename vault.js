1992a1fe6112b842d1bbb50598f932fcd4623534
KanKan <cindykan@KanKan.local>
2018-01-02 18:38:18 -1000

    Completed

--- vault.js
+++ vault.js
@@ -1,4 +1,21 @@
 'use strict';
 module.exports = function() {
-  
-};
+    let myVault = {};
+    let setValue = function (key, value){
+        myVault[key] = value;
+        return myVault;
+    }
+      
+    let getValue = function (key){
+        if(key === undefined || !myVault[key]) {
+            return null;
+        } else {
+            return myVault[key];
+        }
+
+    }
+    return {
+        setValue: setValue,
+        getValue: getValue,
+    }
+  };
